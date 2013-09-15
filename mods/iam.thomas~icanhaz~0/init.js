c0cf6c189673dcabcc0eee73fc4d2dc56d624604
Thomas Steinhauer <thomassteinhauer@gmail.com>
2013-09-15 17:16:41 +0200

    init ()

---
+++ mods/iam.thomas~icanhaz~0/init.js
@@ -0,0 +1,63 @@
+// vertx
+var vertx = require('vertx');
+var container = require('vertx/container');
+var console = require('vertx/console');
+var config = container.config;
+var eb = vertx.eventBus;
+
+
+var address = container.config.address || "template.ich"
+var template_dir = container.config.template_dir || "./templates"
+var ich = require('./lib/ICanHaz.js');
+
+console.log('config is ' + JSON.stringify(container.config));
+
+//console.log(config.template_dir);
+/**
+ * Recursevly scans a dir for files!
+ *
+ * @param path
+ */
+var scanDir = function (path) {
+    vertx.fileSystem.readDir(path, function (err, files) {
+        console.log('Directory "' + path + '" contains these ' + files.length + ' items:');
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            var fileprops = vertx.fileSystem.propsSync(file);
+
+            if (fileprops.isDirectory) {
+                scanDir(file);
+            } else if (fileprops.isRegularFile) {
+
+                // regex to only scan .html files
+                var regex_file_type = /[^\.]+$/gim;
+                if (regex_file_type.exec(file)[0] == "html") {
+
+                    // regex to get filename = template name
+                    var regex_template_name = /([^\/]+)(?=\.\w+$)/gm;
+                    var template_name = regex_template_name.exec(file)[0];
+                    var template_content = vertx.fileSystem.readFileSync(file).toString();
+
+                    // add template to icanhaz
+                    ich.addTemplate(template_name, template_content);
+                    console.log('[+] Template: "' + template_name);
+                }
+            }
+        }
+    });
+};
+
+// init scan
+scanDir(template_dir);
+
+
+var renderWithICHAction = function (param, response) {
+    console.log('[' + address + '] rendering template [' + param.template + '] with data: ' + JSON.stringify(param.data));
+    response(ich[param.template](param.data));
+}
+
+eb.registerHandler(address, renderWithICHAction, function () {
+    console.log('[' + address + '] handler registered!')
+});
+
+
